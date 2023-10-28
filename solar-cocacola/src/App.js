db084c6c06bfa5944bd3e9dab0b62a93a64080d9
Giorgioeab <eltoncr7@hotmail.com>
2023-10-28 13:55:58 -0300

    login page

--- solar-cocacola/src/App.js
+++ solar-cocacola/src/App.js
@@ -1,11 +1,43 @@
+import React, { useState } from 'react';
 import './App.css';
 
 function App() {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log(`Email: ${email}, Senha: ${password}`);
+  };
+  const users = [
+    user:{
+      
+    }
+  ]
   return (
     <div className="App">
-      <header className="App-header">
-
-      </header>
+      <form onSubmit={handleSubmit}>
+        <h1>Login</h1>
+        <div className="input-group">
+          <label htmlFor="email">Email:</label>
+          <input
+            type="email"
+            id="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </div>
+        <div className="input-group">
+          <label htmlFor="password">Senha:</label>
+          <input
+            type="password"
+            id="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+        </div>
+        <button type="submit">Entrar</button>
+      </form>
     </div>
   );
 }
